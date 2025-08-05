6568a77ae8ba7ea07599470c83297222e0c2199d
MayraSanchez <mayra.alejandra.sanchez@correounivalle.edu.com>
2025-08-05 15:14:18 -0500

    add new video

--- src/pages/components/video/HeroVideo.jsx
+++ src/pages/components/video/HeroVideo.jsx
@@ -4,13 +4,13 @@ import "./HeroVideo.css";
 const HeroVideo = () => {
   return (
     <div className="video-container">
-      <iframe
-        src="https://www.youtube.com/embed/KA44sobuBwA?autoplay=1&loop=1&rel=0&modestbranding=1&playlist=KA44sobuBwA&controls=0&showinfo=0&vq=hd1080&mute=1"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <video
+        src="/assets/hero.mp4"
+        autoPlay
+        loop
+        playsInline
+        controls={false}
+      />
     </div>
   );
 };
